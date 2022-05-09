e0dc181144548de3ebeb2361c723277257937386
humberto <humbertoe11@hotmail.com>
2022-05-08 22:51:20 -0300

    agora o usuario eh capaz de cadastrar um time com quantos jogadores ele quiser

--- Aula 2 - Jogador/src/com/company/Jogador.java
+++ Aula 2 - Jogador/src/com/company/Jogador.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.time.LocalDateTime;
+import java.util.Random;
 
 public class Jogador {
     public int id;
@@ -12,13 +13,14 @@ public class Jogador {
     public int qualidade;
     public int cartoesAmarelos;
     public int cartaoVermelho ;
-    public boolean suspenso;
+    public boolean suspenso = false;
+    public boolean treinou = false;
 
     public Jogador(){
 
     }
 
-    public Jogador(int id, String nome, String apelido, LocalDateTime data, int numero, String posicao, int qualidade, int cartoesAmarelos, int cartaoVermelho, boolean suspenso){
+    public Jogador(int id, String nome, String apelido, LocalDateTime data, int numero, String posicao, int qualidade, int cartoesAmarelos, int cartaoVermelho){
         this.id = id;
         this.nome = nome;
         this.apelido = apelido;
@@ -28,7 +30,68 @@ public class Jogador {
         this.qualidade = qualidade;
         this.cartoesAmarelos = cartoesAmarelos;
         this.cartaoVermelho = cartaoVermelho;
-        this.suspenso = suspenso;
+
+    }
+
+    public void aplicarCartaoAmarelo(){
+        this.cartoesAmarelos++;
+        if(this.cartoesAmarelos >= 3){
+            this.suspenso = true;
+        }
+    }
+    public void aplicarCartaoVermelho(){
+        this.cartaoVermelho++;
+        this.suspenso = true;
+    }
+    public void cumprirSuspencao(){
+        this.cartoesAmarelos = 0;
+        this.cartaoVermelho = 0;
+    }
+    public void sofrerLesao(){
+        Random rand = new Random();
+        float qualidadeTemporaria;
+        float nivelDaLesao = rand.nextFloat();
+        System.out.println("Nivel da lesao:"+nivelDaLesao);
+        if(nivelDaLesao <= 0.4){
+            this.qualidade++;
+        }else if(nivelDaLesao <= 0.7){
+            this.qualidade+=2;
+        }else if(nivelDaLesao <= 0.85){
+            qualidadeTemporaria = this.qualidade;
+            qualidadeTemporaria = qualidadeTemporaria - (qualidadeTemporaria*0.95f);
+            this.qualidade = Math.round(qualidadeTemporaria);
+        }else if(nivelDaLesao <= 0.95){
+            qualidadeTemporaria = this.qualidade;
+            qualidadeTemporaria = qualidadeTemporaria - (qualidadeTemporaria*0.90f);
+            this.qualidade = Math.round(qualidadeTemporaria);
+        }else{
+            qualidadeTemporaria = this.qualidade;
+            qualidadeTemporaria = qualidadeTemporaria - (qualidadeTemporaria*0.85f);
+            this.qualidade = Math.round(qualidadeTemporaria);
+        }
+    }
+
+    public void executarTreinamento(){
+        if (!this.treinou){
+            Random rand = new Random();
+            this.qualidade = this.qualidade + rand.nextInt(2) + 1;
+            this.treinou = true;
+        }else{
+            this.qualidade = this.qualidade;
+        }
+    }
+
+    public void gerarRelatorioSobreJogador(){
+        System.out.println("----------------------------------------");
+        System.out.println("Nome do jogador: " + this.nome);
+        System.out.println("Apelido: " + this.apelido);
+        System.out.println("Numero do jogador: " + this.numero);
+        System.out.println("Posição: " + this.posicao);
+        System.out.println("Pontos de qualidade: " + this.qualidade);
+        System.out.println("Numero de cartões amarelos: " + this.cartoesAmarelos);
+        System.out.println("Numero de cartões vermelhos: " + this.cartaoVermelho);
+        System.out.println("Data de cadastro" + this.data.toString());
+        System.out.println("----------------------------------------");
     }
 
 }
--- Aula 2 - Jogador/src/com/company/Main.java
+++ Aula 2 - Jogador/src/com/company/Main.java
@@ -11,12 +11,16 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Digite o nome do Time: ");
     String nomeDoTime = scanner.nextLine();
-    int id, numero, qualidade, cartoesAmarelos, cartaoVermelho;
+    int id = 0, numero, qualidade, cartoesAmarelos, cartaoVermelho;
     String nome, apelido, posicao;
-    boolean suspenso = false;
+
+    System.out.println("Bem vindo " + "! Quantos jogadores tem seu time?");
+
+    int quantidadeDeJogadores = scanner.nextInt();
 
 
-    Jogador[] time = new Jogador[11];
+
+    Jogador[] time = new Jogador[quantidadeDeJogadores];
 
     System.out.println("Vamos cadastrar os 11 jogadores titulares: ");
 
@@ -36,8 +40,46 @@ public class Main {
     cartaoVermelho = scanner.nextInt();
     
 
-    time[0] = new Jogador(id = 0, nome, apelido, data, numero, posicao = "goleiro", qualidade, cartoesAmarelos, cartaoVermelho, suspenso);
+    time[0] = new Jogador(id, nome, apelido, data, numero, posicao = "goleiro", qualidade, cartoesAmarelos, cartaoVermelho);
 
     System.out.println(time[0].id + time[0].nome + time[0].apelido + time[0].data + time[0].numero + time[0].posicao + time[0].qualidade + time[0].cartoesAmarelos + time[0].cartaoVermelho + time[0].suspenso);
+    System.out.println("Cartao Amarelo!");
+    time[0].aplicarCartaoAmarelo();
+    System.out.println("Cartao Vermelho!");
+    time[0].aplicarCartaoVermelho();
+        System.out.println(time[0].id + time[0].nome + time[0].apelido + time[0].data + time[0].numero + time[0].posicao + time[0].qualidade + time[0].cartoesAmarelos + time[0].cartaoVermelho + time[0].suspenso);
+    System.out.println("Cumpriu Suspensao!");
+    time[0].cumprirSuspencao();
+    System.out.println("Sofreu uma lesao!");
+    time[0].sofrerLesao();
+    System.out.println("Hora de treinar!");
+    time[0].executarTreinamento();
+
+        System.out.println(time[0].id + time[0].nome + time[0].apelido + time[0].data + time[0].numero + time[0].posicao + time[0].qualidade + time[0].cartoesAmarelos + time[0].cartaoVermelho + time[0].suspenso);
+
+    System.out.println("Agora que você cadastrou o goleiro, vamos cadastrar o resto do time!");
+    for(int i = 1; i < quantidadeDeJogadores; i++){
+        System.out.println("Digite o nome desse jogador: ");
+        nome = scanner.nextLine();
+        System.out.println("Digite o apelido desse jogador: ");
+        apelido = scanner.nextLine();
+        data = LocalDateTime.now();
+        System.out.println("Digite o numero desse jogador: ");
+        numero = scanner.nextInt();
+        System.out.println("Digite qual a posicao do jogador: ");
+        posicao = scanner.nextLine();
+        System.out.println("Digite os pontos de qualidade do jogador: ");
+        qualidade = scanner.nextInt();
+        System.out.println("Digite a quantidade de cartões amarelos atuais do jogador: ");
+        cartoesAmarelos = scanner.nextInt();
+        System.out.println("Digite a quantidade de cartões vermelhos atuais do jogador:");
+        cartaoVermelho = scanner.nextInt();
+        id++;
+        time[i] = new Jogador(id = id+1, nome, apelido, data, numero, posicao, qualidade, cartoesAmarelos, cartaoVermelho);
+        System.out.println("Jogador Cadastrado com Sucesso!");
     }
+
+
+    }
+
 }
