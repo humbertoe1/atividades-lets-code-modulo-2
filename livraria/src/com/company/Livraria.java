5f357852c677a56e067c0cd4edd102964ac91533
humbertoe1 <humbertoe11@hotmail.com>
2022-06-09 13:21:40 -0300

    Agora uma compra que passe de 200,00 recebe um desconto de 15%

--- livraria/src/com/company/Livraria.java
+++ livraria/src/com/company/Livraria.java
@@ -4,18 +4,30 @@ import java.math.BigDecimal;
 
 public class Livraria {
 
-    public Caixa caixa;
-    public Estoque estoque;
+    private Caixa caixa;
+    private Estoque estoque;
+    private final BigDecimal VALOR_PROMOCIONAL = new BigDecimal("200");
+    private final BigDecimal DESCONTO = new BigDecimal("0.85");
 
-    public Livraria(){
+    public Livraria() {
         caixa = new Caixa(BigDecimal.ZERO);
         estoque = new Estoque();
     }
 
-    public void venda(Produto produto, int quantidade){
-        BigDecimal valorPago = new BigDecimal("quantidade").multiply(produto.getPreco());
+    public void venda(Produto produto, int quantidade) {
+        BigDecimal valorPago = calcularValorPago(produto.getPreco(), quantidade);
+
         caixa.somaValor(valorPago);
         estoque.alterarQuantidadeProduto(produto, quantidade);
+    }
+
+    private BigDecimal calcularValorPago(BigDecimal valor, int quantidade) {
+
+        BigDecimal valorPago = new BigDecimal("quantidade").multiply(valor);
+        if (valorPago.compareTo(VALOR_PROMOCIONAL) >= 0) {
+            valorPago = valorPago.multiply(DESCONTO);
+        }
+        return valorPago;
     }
 
 }
